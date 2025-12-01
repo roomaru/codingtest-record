8c6e0b4411e11edd71e553b6b9e0f55e816105ad
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2025-12-01 15:39:32 +0900

    Update 큰 수 만들기.js

--- 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
+++ 프로그래머스/2/42883. 큰 수 만들기/큰 수 만들기.js
@@ -1,8 +1,10 @@
 function solution(number, k) {
     var res = [];
     res.push(number[0]);
-    
+
     for (let i = 1; i < number.length; i++) {
+        // 앞자리 숫자를 최대한 크게 가져가야 함
+        // 마지막 값보다 작거나 같으면 그냥 넣고, 마지막 값보다 크면 마지막 값을 제거하고 넣어줌
         while (k > 0 && res[res.length-1] < number[i]) {
             res.pop();
             k--;
