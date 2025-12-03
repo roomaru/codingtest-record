a58403207723f91977790c435e3398c3e6befcdd
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2025-12-03 11:15:28 +0900

    Update 의상.js

--- 프로그래머스/2/42578. 의상/의상.js
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -7,6 +7,6 @@ function solution(clothes) {
     }
     
     // n번 째 의상을 입는 경우 + 아예 안입는 경우 -> (n+1)
-    // 의상마다의 경우를 모두 곱함
+    // 의상마다의 경우를 모두 곱함 - 전부 안입는 경우
     return Object.values(counts).reduce((a, b) => a*(b+1), 1)-1;
 }
