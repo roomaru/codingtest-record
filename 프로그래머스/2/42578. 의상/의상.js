3afd621ade62e7467dd5ec025a2dab910bd52bf1
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2025-12-03 11:14:16 +0900

    [level 2] Title: 의상, Time: 0.26 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.js
@@ -0,0 +1,12 @@
+function solution(clothes) {
+    const counts = {};
+    
+    for (let i = 0; i < clothes.length; i++) {
+        const key = clothes[i][1];
+        counts[key] = (counts[key] || 0) + 1;
+    }
+    
+    // n번 째 의상을 입는 경우 + 아예 안입는 경우 -> (n+1)
+    // 의상마다의 경우를 모두 곱함
+    return Object.values(counts).reduce((a, b) => a*(b+1), 1)-1;
+}
