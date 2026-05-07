108215890e36e30eb90a47f282f7bf6afa5812ab
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2026-05-07 14:59:44 +0900

    Enhance comments for solution function
    Added improved code comments for clarity.

--- 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
+++ 프로그래머스/1/138477. 명예의 전당 （1）/명예의 전당 （1）.js
@@ -15,3 +15,23 @@ function solution(k, score) {
     
     return answer;
 }
+
+// 개선 코드
+// function solution(k, score) {
+//     const answer = [];
+//     const fame = [];
+    
+//     for (const s of score) {
+//         if (fame.length < k || s > fame[fame.length - 1]) {
+//             fame.push(s);
+//             fame.sort((a, b) => b-a);
+
+//             if (fame.length > k) {
+//                 fame.pop();
+//             }
+//         }
+//         answer.push(fame[fame.length - 1]);
+//     }
+    
+//     return answer;
+// }
