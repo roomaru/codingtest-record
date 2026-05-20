5229b76986f596fe4f677319183947e9fd05283a
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2026-05-20 10:22:52 +0900

    [level 1] Title: 모의고사, Time: 2.61 ms, Memory: 35.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.js
+++ 프로그래머스/1/42840. 모의고사/모의고사.js
@@ -2,21 +2,19 @@ function solution(answers) {
     var scores = [0, 0, 0];
     var answer = [];
     
-    const a = [1, 2, 3, 4, 5];
-    const b = [2, 1, 2, 3, 2, 4, 2, 5];
-    const c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
+    const num1 = [1, 2, 3, 4, 5];
+    const num2 = [2, 1, 2, 3, 2, 4, 2, 5];
+    const num3 = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5];
     
     for (let i = 0; i < answers.length; i++) {
-        if (answers[i] === a[i%5]) scores[0] += 1;
-        if (answers[i] === b[i%8]) scores[1] += 1;
-        if (answers[i] === c[i%10]) scores[2] += 1;
+        if (answers[i] === num1[i%5]) scores[0]++;
+        if (answers[i] === num2[i%8]) scores[1]++;
+        if (answers[i] === num3[i%10]) scores[2]++;
     }
     
-    const max_score = Math.max(...scores);
+    const maxScore = Math.max(...scores);
     for (let i = 0; i < 3; i++) {
-        if (scores[i] === max_score) {
-            answer.push(i+1);
-        }
+        if (scores[i] === maxScore) answer.push(i+1);
     }
     
     return answer;
