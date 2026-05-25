831c52dced45b828a817e2ecfbc5a0ea6e635bca
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2026-05-25 10:28:46 +0900

    Refactor cake cutting solution for performance
    Updated the solution to avoid time limit issues by removing inefficient slice and Set operations.

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.js
@@ -25,3 +25,19 @@ function solution(topping) {
     
     return answer;
 }
+
+// 틀린 코드: 정확성은 맞으나 시간 초과 -> 반복문마다 slice() + Set()을 새로 만들기 때문 (O(N^2))
+// function solution(topping) {
+//     var answer = 0;
+    
+//     for (let i = 1; i < topping.length; i++) {
+//         let cake1 = [...new Set(topping.slice(0, i))];
+//         let cake2 = [...new Set(topping.slice(i))];
+        
+//         if (cake1.length === cake2.length) answer++;
+//     }
+    
+//     return answer;
+// }
+
+
