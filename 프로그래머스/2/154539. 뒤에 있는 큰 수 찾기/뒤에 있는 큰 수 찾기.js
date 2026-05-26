2e96708451446064d478117e2e66ce616b061f03
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2026-05-26 10:44:34 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 28.80 ms, Memory: 156 MB -BaekjoonHub

---
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -0,0 +1,19 @@
+function solution(numbers) {
+    var answer = Array(numbers.length).fill(-1);
+    var stack = []; // 아직 뒷 큰수를 못 찾은 인덱스 저장
+    
+    for (let i = 0; i < numbers.length; i++) {
+        while (
+            stack.length > 0 &&
+            numbers[stack[stack.length - 1]] < numbers[i]
+        ) {
+            const idx = stack.pop();
+            answer[idx] = numbers[i];
+        }
+        
+        stack.push(i);
+    }
+    
+    return answer;
+}
+
