b7e0e4ab2a18f3588fda72871aa597109ad75294
DAYEON RYU <127957950+roomaru@users.noreply.github.com>
2026-05-31 10:49:59 +0900

    Optimize solution for finding next greater number

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.js
@@ -1,18 +1,40 @@
 function solution(numbers) {
     var answer = Array(numbers.length).fill(-1);
-    var idx_stack = [];
+    var stack = []; // 아직 뒷 큰수를 못 찾은 인덱스 저장
     
     for (let i = 0; i < numbers.length; i++) {
         while (
-            idx_stack.length > 0 &&
-            numbers[idx_stack[idx_stack.length - 1]] < numbers[i]
+            stack.length > 0 &&
+            numbers[stack[stack.length - 1]] < numbers[i]
         ) {
-            const idx = idx_stack.pop();
+            const idx = stack.pop();
             answer[idx] = numbers[i];
         }
         
-        idx_stack.push(i);
+        stack.push(i);
     }
     
     return answer;
 }
+
+// 틀린 함수: 시간 초과(이중 포문)
+// function solution(numbers) {
+//     var answer = [];
+    
+//     for (let i = 0; i < numbers.length; i++) {
+//         var flag = false;
+        
+//         for (let j = i+1; j < numbers.length; j++) {
+//             if (numbers[i] < numbers[j]) {
+//                 answer.push(numbers[j]);
+//                 flag = true;
+//                 break;
+//             } else {
+//                 flag = false;
+//             }
+//         }
+//         if (!flag) answer.push(-1)
+//     }
+    
+//     return answer;
+// }
